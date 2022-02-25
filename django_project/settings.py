14d4eaeac26e0d6537ca47b12234e3f37df1c0d0
airncodes <erin@entaylor.io>
2022-02-25 11:01:53 -0800

    Changed secret key information in settings.py to keep off github

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lf70cw4v5!7td078hh32c4twc5j02&y&j&rkva8=dsliw6obgl'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
